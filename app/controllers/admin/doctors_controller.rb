1fbef32721f62406b8c4be3867085f747a370fb4
Rakesh Rao <rrao@anypresence.com>
2012-11-17 16:30:21 -0500

    Generated v1 API.

---
+++ app/controllers/admin/doctors_controller.rb
@@ -0,0 +1,92 @@
+class Admin::DoctorsController < ApplicationController
+  before_filter :authenticate_admin!
+  layout 'admin'
+
+  # GET /doctors
+  # GET /doctors.json
+  def index
+    @doctors = V1::Doctor.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @doctors }
+    end
+  end
+
+  # GET /doctors/1
+  # GET /doctors/1.json
+  def show
+    
+    @doctor = V1::Doctor.get(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @doctor }
+    end
+  end
+
+  # GET /doctors/new
+  # GET /doctors/new.json
+  def new
+    @doctor = V1::Doctor.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @doctor }
+    end
+  end
+
+  # GET /doctors/1/edit
+  def edit
+    
+    @doctor = V1::Doctor.get(params[:id])
+    
+  end
+
+  # POST /doctors
+  # POST /doctors.json
+  def create
+    @doctor = V1::Doctor.new(params[:doctor], as: :"System Admin")
+    respond_to do |format|
+      if @doctor.save
+        format.html { redirect_to admin_doctor_url(@doctor), notice: 'doctor was successfully created.' }
+        format.json { render json: @doctor, status: :created, location: admin_doctor_url(@doctor) }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @doctor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /doctors/1
+  # PUT /doctors/1.json
+  def update
+    
+    @doctor = V1::Doctor.get(params[:id])
+    
+    @doctor.update_attributes(params[:doctor], as: :"System Admin")
+    respond_to do |format|
+      if @doctor.save
+        format.html { redirect_to admin_doctor_url(@doctor), notice: 'doctor was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @doctor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /doctors/1
+  # DELETE /doctors/1.json
+  def destroy
+    
+    @doctor = V1::Doctor.get(params[:id])
+    
+    @doctor.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_doctors_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTemplate::Application.config.secret_token = 'fc5c2c8c53d9bee88f37c4a0cdb643174a9daa8384cf3be604ff904f2aae8943767d295860619784cea68e76d6017b22e5ae709647c132d8f569e030dab2e57993d0ed7677f3fc67fa8974c3d6e6e016e26ac731d8fd2695f801146cd747398ccfce1a0102080a07d1ae9c419c3aa68c78cca5ee8d78852567d84e196bee939c'
